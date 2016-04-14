7427ad50676db2a05ace8210e93f915c0c0ffcb6
Eric Humpert <EricMac@ricks-mbp.apollosrc.root>
2016-04-14 15:24:16 -0500

    blank slate point for practice entry

---
+++ script.js
@@ -0,0 +1,16 @@
+var searchValue = document.querySelector('#search-value');
+var foodList = document.querySelector('#food-list');
+
+var food = [
+  'Tacos',
+  'Shrimp',
+  'Steak',
+  'Pizza',
+  'Tex-Mex',
+  'Po-Boy',
+  'Hamburger',
+  'French-fries',
+  'Brisket',
+  'Ribs',
+  'Pulled-pork'
+];
