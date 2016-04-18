811dcb96455cf1385181145de0c00b0f142bed17
Eric Humpert <EricMac@Ricks-MBP.attlocal.net>
2016-04-17 19:18:32 -0500

    wrote the entire functional aspect of the code without looking at outside reference materials.

--- script.js
+++ script.js
@@ -17,8 +17,34 @@ var food = [
 
 //DOM list builder function
 
+function _foodListDOMBuilder(arr) {
+  for (var val of arr) {
+    var li = document.createElement('li');
+    li.textContent = val;
+
+    foodList.appendChild(li);
+  }
+}
+
 //call the DOM list builder
+
+_foodListDOMBuilder(food);
 
 //DOM user input search function
 
+function _foodListUserInputSearch(evt) {
+  var query = searchValue.value;
+
+  var filteredList = [];
+
+  for (var val of food) {
+    if(val.indexOf(query) > -1) {
+    filteredList.push(val);
+    }
+  }
+  foodList.innerHTML = '';
+  _foodListDOMBuilder(filteredList)
+}
+
 //Call the DOM user input function
+searchValue.addEventListener('keyup', _foodListUserInputSearch)
